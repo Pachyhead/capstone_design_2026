5d666333784858744269f610f0410027e69366b2
KyeongTaek <tommyholic@naver.com>
2026-04-29 23:26:26 +0900

    enable communication from remote

--- grpc-getting-started/server_communicate_sender.py
+++ grpc-getting-started/server_communicate_sender.py
@@ -13,7 +13,7 @@ from dotenv import load_dotenv
 
 def run():
     """
-    Codelab Hint: Logic for your gRPC Client will be added here.
+    Codelab Hint: Logic for your gRPC sender Client will be added here.
     Steps include:
      1. Create a connection to the gRPC server using grpc.insecure_channel()
      2. Call service methods on the client to interact with the server.
@@ -25,12 +25,11 @@ def run():
     
     uploadRequest = server_communicate_pb2.SpeechUploadRequest(sender_id="sendR", receiver_id="receiveR", text="anonymous text", emotion_vector=[0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.72]) # 서비스를 호출할 SpeechUploadRequest 정의
     status = stub.UploadSpeechTask(uploadRequest) # 서버 메서드 호출(rpc 호출)
-    print(status)
-
+    
     if status.accepted:
-        print(f"Upload success '{status.task_id}'")
+        print(f"Upload success!")
     else:
-        printf(f"Upload faild")    
+        printf(f"Upload failed!")    
 
 
 if __name__ == "__main__":
--- grpc-getting-started/use_e2v_text_data.py
+++ grpc-getting-started/use_e2v_text_data.py
@@ -1,17 +1,25 @@
-def doSomething():
+import json
+
+def doSomething(request):
     try:
-        with open("sample1.json", "w", encoding="utf-8") as f:
-            f.write("{\n")
-            f.write("\t\"location\": {\n")
-            f.write("\t\t\"latitude\": 407838,\n")
-            f.write("\t\t\"longitude\": -74614\n")
-            f.write("\t},\n")
-            f.write("\t\"name\": \"Patriots Path, USA\"\n")
-            f.write("}")
+        fname = f"{request.receiver_id}.json"
+
+        with open(fname, "w", encoding="utf-8") as f:
+            f.write(f"{\n")
+            f.write(f"\t\"sender_id\": {request.sender_id},\n")
+            f.write(f"\t\"receiver_id\": {request.receiver_id},\n")
+            f.write(f"\t\"text\": {request.text},\n")
+            f.write(f"\t\"emotion_vector\": {request.emotion_vector}\n")
+            f.write(f"}")
             return True
     except OSError as e:
         print(f"File write error: {e}")
         return False
 
-def get_pending_message():
-    return "pended message"
+def get_pending_message(user_id):
+    fname = f"{user_id}.json"
+
+    with open(fname, "r") as f:
+        json_data = json.load(f)
+        
+    return json_data['text']
