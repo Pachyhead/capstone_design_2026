929cba28e5e5a75b9de9bb2431e4b04b3fc6409c
Pachyhead <bitcoin-groove0a@icloud.com>
2026-05-06 00:03:06 +0900

    create sender skeleton

---
+++ sender/actions.py
@@ -0,0 +1,34 @@
+from argparse import ArgumentParser
+
+from sender.config import SERVER_IP, SERVER_PORT
+from sender.inference import speech2emovec, speech2text
+
+import torch
+
+def _parse_args():
+    parser = ArgumentParser()
+
+    parser.add_argument()
+
+    return parser.parse_args
+
+def _make_packet(message: str, emovec: torch.Tensor) -> dict[str, str]:
+    packet: dict[str, str] = {}
+
+    return packet
+
+def _record():
+    text = speech2text()
+    emovec = speech2emovec()
+    return text, emovec
+
+def send():
+    text, emovec = _record()
+    print("준비 완료")
+    packet = _make_packet(text, emovec)
+    pass
+
+if __name__ == "__main__":
+    args = _parse_args()
+
+    send()
