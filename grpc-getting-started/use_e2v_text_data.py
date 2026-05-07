73498fa32822138f1c59efcf352dd1cae7f4a826
KyeongTaek <tommyholic@naver.com>
2026-05-07 11:00:46 +0900

    feat: Modify server code to fit plan
    Modified server code to fit backend structure diagram
    
    follow: #7
    
    Signed-off-by: KyeongTaek <tommyholic@naver.com>

--- grpc-getting-started/use_e2v_text_data.py
+++ grpc-getting-started/use_e2v_text_data.py
@@ -1,6 +1,6 @@
 import json
 
-def doSomething(request):
+def doSomething(request): # 받은 SpeechUploadRequest 내용을 수신자id.json으로 서버에 저장
     try:
         fname = f"{request.receiver_id}.json"
 
@@ -8,6 +8,7 @@ def doSomething(request):
             f.write("{\n")
             f.write(f"\t\"sender_id\": \"{request.sender_id}\",\n")
             f.write(f"\t\"receiver_id\": \"{request.receiver_id}\",\n")
+            f.write(f"\t\"msg_id\": \"{request.msg_id}\",\n")
             f.write(f"\t\"text\": \"{request.text}\",\n")
             f.write(f"\t\"emotion_vector\": {request.emotion_vector}\n")
             f.write("}")
@@ -16,10 +17,18 @@ def doSomething(request):
         print(f"File write error: {e}")
         return False
 
-def get_pending_message(user_id):
-    fname = f"../{user_id}.wav"
+def getVoice(request): # 받은 MessageIdentifier 정보에 해당하는 wav를 찾아 바이트 형태로 반환
+    fname = f"../{request.message_id}.wav"
 
     with open(fname, "rb") as f:
         wav_bytes = f.read()
 
     return wav_bytes
+
+def getMetadata(user_id):
+    fname = f"{user_id}.json"
+
+    with open(fname, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    
+    return [data['msg_id'], data['sender_id'], data['text']]
