01d46723c97cc5c7e6014b2b05c359dc91164d10
Pachyhead <bitcoin-groove0a@icloud.com>
2026-05-09 18:37:01 +0900

    implement make_packet() and send()

--- sender/actions.py
+++ sender/actions.py
@@ -1,11 +1,12 @@
 from argparse import ArgumentParser
 
-from sender.config import SERVER_IP, SERVER_PORT, SENDER_ROOT
+from sender.config import SERVER_IP, SERVER_PORT, SENDER_ROOT, MESSEGE_ID, USER_ID
 from sender.inference import speech2emovec, speech2text
 
+import requests
 import torch
 
-audio_storage = SENDER_ROOT / "storage"
+audio_storage = SENDER_ROOT / "temp_storage"
 
 def _parse_args():
     parser = ArgumentParser()
@@ -14,8 +15,14 @@ def _parse_args():
 
     return parser.parse_args
 
-def _make_packet(message: str, emovec: torch.Tensor) -> dict[str, str]:
+def _make_packet(user_id: str, message_id: str, message: str, emo_embed: str) -> dict[str, str]:
+    # 추후 user_id와 message_id 검증 로직 필요.
     packet: dict[str, str] = {}
+
+    packet["User_Id"] = user_id
+    packet["Message_Id"] = message_id
+    packet["Message"] = message
+    packet["emo_embed"] = emo_embed
 
     return packet
 
@@ -26,9 +33,15 @@ def _record():
 
 def send():
     text, emovec = _record()
-    print("준비 완료")
-    packet = _make_packet(text, emovec)
-    pass
+    print("successfully recorded")
+    packet = _make_packet(USER_ID, MESSEGE_ID, text, emovec)
+    
+    response = requests.post(
+    f"{SERVER_IP}:{SERVER_PORT}/send",
+    json=packet
+    )
+    print(response.status_code)
+    print(response.text)
 
 if __name__ == "__main__":
     args = _parse_args()
