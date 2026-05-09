49a3a609f3fd1b66efa3241e1b4350de6cb6e2be
Pachyhead <bitcoin-groove0a@icloud.com>
2026-05-09 19:14:43 +0900

    implement sender process

--- sender/actions.py
+++ sender/actions.py
@@ -2,12 +2,11 @@ from argparse import ArgumentParser
 from datetime import datetime
 
 from sender.config import SERVER_IP, SERVER_PORT, SENDER_ROOT, MESSEGE_ID, USER_ID
-# from sender.inference import speech2emovec, speech2text
+from sender.inference import speech2emovec, speech2text
 
 import sounddevice as sd
 from scipy.io.wavfile import write
-# import requests
-# import torch
+import requests
 
 audio_storage = SENDER_ROOT / "storage"
 audio_storage.mkdir(parents=True, exist_ok=True)
@@ -16,7 +15,7 @@ def _parse_args():
     parser = ArgumentParser()
     parser.add_argument()
 
-    return parser.parse_args
+    return parser.parse_args()
 
 def _make_packet(user_id: str, message_id: str, message: str, emo_embed: str) -> dict[str, str]:
     # 추후 user_id와 message_id 검증 로직 필요.
@@ -29,7 +28,7 @@ def _make_packet(user_id: str, message_id: str, message: str, emo_embed: str) ->
 
     return packet
 
-def record_audio(save_dir=audio_storage, duration=5, sample_rate=44100, channels=1):
+def _record(save_dir=audio_storage, duration=5, sample_rate=44100, channels=1):
     # 파일 이름 생성
     filename = datetime.now().strftime("recording_%Y%m%d_%H%M%S.wav")
     file_path = save_dir / filename
@@ -53,24 +52,31 @@ def record_audio(save_dir=audio_storage, duration=5, sample_rate=44100, channels
 
     return file_path
 
-def _record():
-    text = speech2text()
-    emovec = speech2emovec()
+def _send(packet: dict[str, str]):
+    response = requests.post(
+    f"{SERVER_IP}:{SERVER_PORT}/send",
+    json=packet
+    )
+    print(response.status_code)
+    print(response.text)
+
+def process():
+    audio_file = _record(duration=10)
+
+    if not (text := speech2text(str(audio_file))):
+        print("STT에서 에러 발생")
+        exit(1)
+    if not (emovec := speech2emovec(str(audio_file))):
+        print("emo2vec에서 에러 발생")
+        exit(1)
+    # emovec 압축 로직 필요
+
+    packet = _make_packet(USER_ID, MESSEGE_ID, text, emovec)
+
+    _send(packet=packet)
+
     return text, emovec
 
-# def send():
-#     text, emovec = _record()
-#     print("successfully recorded")
-#     packet = _make_packet(USER_ID, MESSEGE_ID, text, emovec)
-    
-#     response = requests.post(
-#     f"{SERVER_IP}:{SERVER_PORT}/send",
-#     json=packet
-#     )
-#     print(response.status_code)
-#     print(response.text)
 
 if __name__ == "__main__":
-    record_audio(duration=10)
-    # args = _parse_args()
-    # send()
+    process()
