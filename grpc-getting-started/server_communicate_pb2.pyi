4003b58dc8620a290873c2efaea055236b610076
KyeongTaek <tommyholic@naver.com>
2026-05-14 23:23:58 +0900

    fix rpc message

--- grpc-getting-started/server_communicate_pb2.pyi
+++ grpc-getting-started/server_communicate_pb2.pyi
@@ -1,23 +1,27 @@
+import datetime
+
+from google.protobuf import timestamp_pb2 as _timestamp_pb2
 from google.protobuf.internal import containers as _containers
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
-from typing import ClassVar as _ClassVar, Iterable as _Iterable, Optional as _Optional
+from collections.abc import Iterable as _Iterable, Mapping as _Mapping
+from typing import ClassVar as _ClassVar, Optional as _Optional, Union as _Union
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
 class SpeechUploadRequest(_message.Message):
-    __slots__ = ("sender_id", "receiver_id", "msg_id", "text", "emotion_vector")
+    __slots__ = ("sender_id", "receiver_id", "message", "emo_type", "emotion_vector")
     SENDER_ID_FIELD_NUMBER: _ClassVar[int]
     RECEIVER_ID_FIELD_NUMBER: _ClassVar[int]
-    MSG_ID_FIELD_NUMBER: _ClassVar[int]
-    TEXT_FIELD_NUMBER: _ClassVar[int]
+    MESSAGE_FIELD_NUMBER: _ClassVar[int]
+    EMO_TYPE_FIELD_NUMBER: _ClassVar[int]
     EMOTION_VECTOR_FIELD_NUMBER: _ClassVar[int]
     sender_id: str
     receiver_id: str
-    msg_id: str
-    text: str
+    message: str
+    emo_type: int
     emotion_vector: _containers.RepeatedScalarFieldContainer[float]
-    def __init__(self, sender_id: _Optional[str] = ..., receiver_id: _Optional[str] = ..., msg_id: _Optional[str] = ..., text: _Optional[str] = ..., emotion_vector: _Optional[_Iterable[float]] = ...) -> None: ...
+    def __init__(self, sender_id: _Optional[str] = ..., receiver_id: _Optional[str] = ..., message: _Optional[str] = ..., emo_type: _Optional[int] = ..., emotion_vector: _Optional[_Iterable[float]] = ...) -> None: ...
 
 class UploadStatus(_message.Message):
     __slots__ = ("accepted",)
@@ -31,6 +35,26 @@ class UserIdentifier(_message.Message):
     user_id: str
     def __init__(self, user_id: _Optional[str] = ...) -> None: ...
 
+class MetadataItem(_message.Message):
+    __slots__ = ("message_id", "sender_id", "message", "emo_type", "send_time")
+    MESSAGE_ID_FIELD_NUMBER: _ClassVar[int]
+    SENDER_ID_FIELD_NUMBER: _ClassVar[int]
+    MESSAGE_FIELD_NUMBER: _ClassVar[int]
+    EMO_TYPE_FIELD_NUMBER: _ClassVar[int]
+    SEND_TIME_FIELD_NUMBER: _ClassVar[int]
+    message_id: str
+    sender_id: str
+    message: str
+    emo_type: int
+    send_time: _timestamp_pb2.Timestamp
+    def __init__(self, message_id: _Optional[str] = ..., sender_id: _Optional[str] = ..., message: _Optional[str] = ..., emo_type: _Optional[int] = ..., send_time: _Optional[_Union[datetime.datetime, _timestamp_pb2.Timestamp, _Mapping]] = ...) -> None: ...
+
+class MetadataResponse(_message.Message):
+    __slots__ = ("items",)
+    ITEMS_FIELD_NUMBER: _ClassVar[int]
+    items: _containers.RepeatedCompositeFieldContainer[MetadataItem]
+    def __init__(self, items: _Optional[_Iterable[_Union[MetadataItem, _Mapping]]] = ...) -> None: ...
+
 class MessageIdentifier(_message.Message):
     __slots__ = ("message_id", "sender_id", "receiver_id")
     MESSAGE_ID_FIELD_NUMBER: _ClassVar[int]
@@ -40,16 +64,6 @@ class MessageIdentifier(_message.Message):
     sender_id: str
     receiver_id: str
     def __init__(self, message_id: _Optional[str] = ..., sender_id: _Optional[str] = ..., receiver_id: _Optional[str] = ...) -> None: ...
-
-class MessageMetadata(_message.Message):
-    __slots__ = ("message_id", "sender_id", "text")
-    MESSAGE_ID_FIELD_NUMBER: _ClassVar[int]
-    SENDER_ID_FIELD_NUMBER: _ClassVar[int]
-    TEXT_FIELD_NUMBER: _ClassVar[int]
-    message_id: str
-    sender_id: str
-    text: str
-    def __init__(self, message_id: _Optional[str] = ..., sender_id: _Optional[str] = ..., text: _Optional[str] = ...) -> None: ...
 
 class AudioFrame(_message.Message):
     __slots__ = ("audio_content", "sender_id", "message_id", "is_final")
