9e1578d73cbc71a93c0dd9cdc046a48c036f298c
종찬 <bitcoin-groove0a@icloud.com>
2026-05-26 18:40:02 +0900

    fix pb2_grpc import path

--- sender/sender_src/grpc_getting_started/server_communicate_pb2_grpc.py
+++ sender/sender_src/grpc_getting_started/server_communicate_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-import server_communicate_pb2 as server__communicate__pb2
+from . import server_communicate_pb2 as server__communicate__pb2
 
 GRPC_GENERATED_VERSION = '1.70.0'
 GRPC_VERSION = grpc.__version__
